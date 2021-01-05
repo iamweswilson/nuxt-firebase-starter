a868d8565bfc28dfb5df18c97cca42b9630275f2
Wes Wilson <wes@iamweswilson.com>
2021-01-05 14:27:50 -0800

    a bit more about this project

--- pages/about.vue
+++ pages/about.vue
@@ -2,9 +2,17 @@
   <div>
     <h1 class="text-4xl text-blue-900 font-bold">About</h1>
     <p class="text-gray-700">
-      This is a sample template built on <a class="underline font-bold" href="https://nuxtjs.org/">Nuxt</a> integrated with <a class="underline font-bold" href="https://firebase.google.com/">Firebase</a> auth. Firebase storage and firestore also integegrated, but not working in the demo. BYOF (Bring Your Own Firebase to use). Including <a class="underline font-bold" href="https://tailwindcss.com/">Tailwind CSS</a> and <a class="underline font-bold" href="https://heroicons.com/">Heroicons</a>.
+      This is a sample template built on <a class="underline font-bold" href="https://nuxtjs.org/">Nuxt</a> integrated with <a class="underline font-bold" href="https://firebase.google.com/">Firebase</a> auth. Firebase storage and firestore also integegrated, but not implemented in this demo outside of profile pic storage. BYOF (Bring Your Own Firebase to use). Including <a class="underline font-bold" href="https://tailwindcss.com/">Tailwind CSS</a> and <a class="underline font-bold" href="https://heroicons.com/">Heroicons</a>.
     </p>
-    <p class="mt-8 text-gray-700"><a class="underline font-bold" href="https://github.com/iamweswilson/nuxt-firebase-starter">Grab the code or make it better</a></p>
+    <p class="mt-6 text-gray-700">
+      I decided to put this project out into the wild because while I found a lot of help in the process, I found it difficult to find a resource that laid out this implementation super clearly, incuding name and profile pic management. Keep in mind that I'm still fairly new at this, so there are likely improvements to be made... perhaps by you
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>.
+        <a href="https://iamweswilson.com/blog/nuxt-firebase-starter-template" class="underline font-bold">I wrote about this</a> if you're interested in reading more.
+    </p>
+    <p class="mt-6 text-gray-700"><a class="underline font-bold" href="https://github.com/iamweswilson/nuxt-firebase-starter">Create a new account</a> to try it out or <a class="underline font-bold" href="https://github.com/iamweswilson/nuxt-firebase-starter">see the repo</a> to grab the code or make it better.
+    </p>
     <p class="mt-8">
       <img src="/thanks.gif" alt="">
     </p>
